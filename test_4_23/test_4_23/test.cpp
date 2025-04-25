69250003653062f29edb1158b2b82b57e9b4b08c
muyi-2580 <m19112386564@163.com>
2025-04-25 23:08:14 +0800

    完整了相关功能

--- test_4_23/test_4_23/test.cpp
+++ test_4_23/test_4_23/test.cpp
@@ -1,9 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
-struct Node 
+struct Node
 {
-	Node(int data_): data(data_),next(nullptr){}
+	Node(int data_) : data(data_), next(nullptr) {}
 	int data;
 	Node* next;
 };
@@ -17,24 +19,128 @@ public:
 	{
 		head = new Node(data_);
 	}
-	~Clink() {};
+	~Clink()
+	{
+		while (head != nullptr)
+		{
+			Node* tmp = head->next;
+			delete head;
+			head = tmp;
+		}
+	}
+	// 尾插
 	void inserttail(int data_)
 	{
 		Node* p = head;
-		while (p != nullptr)
+		while (p->next != nullptr)
 		{
 			p = p->next;
 		}
-		p->next = new Node(data_);;
+		p->next = new Node(data_);
 	}
+	// 头插
 	void inserthead(int data_)
 	{
-
+		Node* node = new Node(data_);
+		node->next = head->next;
+		head->next = node;
+	}
+	// 删除节点
+	void remove(int data_)
+	{
+		Node* p = head->next;
+		Node* q = head;	//指向p所指向的上一个节点。
+		while (p != nullptr)
+		{
+			if (p->data != data_)
+			{
+				q = p;
+				p = p->next;
+			}
+			else
+			{
+				q->next = p->next;
+				delete p;
+				return;
+			}
+		}
+	}
+	// 删除多个节点
+	void removeall(int data_)
+	{
+		Node* p = head->next;
+		Node* q = head;	//q是p前一个节点的指针
+		while (p != nullptr)
+		{
+			if (p->data != data_)
+			{
+				q = p;
+				p = p->next;
+			}
+			else
+			{
+				q->next = p->next;
+				delete p;
+				p = q->next;
+			}
+		}
+	}
+	// 查找
+	bool find(int data_)
+	{
+		Node* p = head->next;
+		while (p != nullptr)
+		{
+			if (p->data == data_)
+			{
+				return true;
+			}
+			else
+			{
+				p = p->next;
+			}
+		}
+		return false;
+	}
+	// 打印数据
+	void show()
+	{
+		Node* p = head->next;
+		while (p != nullptr)
+		{
+			cout << p->data << ' ';
+			p = p->next;
+		}
+		cout << endl;
 	}
 };
 
+
 int main()
 {
-
+	srand((unsigned)time(0));
+	Clink link(0);
+	for (int i = 0; i < 10; i++)
+	{
+		int num = rand() % 100;
+		link.inserthead(num);
+	}
+	cout << endl;
+	//link.show();
+	//link.inserthead(8);
+	//link.show();
+	//link.remove(8);
+	//link.show();
+	//link.inserttail(999);
+	//link.inserttail(999);
+	//link.inserttail(999);
+	//link.inserttail(999);
+	//link.show();
+	//link.removeall(999);
+	//link.show();
+	link.inserttail(666);
+	link.show();
+	cout << link.find(666) << endl;
+	cout << link.find(888) << endl;
 	return 0;
 }
