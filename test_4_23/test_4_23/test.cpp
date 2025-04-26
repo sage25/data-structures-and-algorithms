ebacb55e2bc811c3a7ba91569df94e14de090b7d
muyi-2580 <m19112386564@163.com>
2025-04-26 23:16:42 +0800

    增加了功能

--- test_4_23/test_4_23/test.cpp
+++ test_4_23/test_4_23/test.cpp
@@ -113,7 +113,56 @@ public:
 		}
 		cout << endl;
 	}
+public:
+	friend void reverse(Clink& link);
+	friend bool countdown(int k, Clink& link, int& val);
 };
+
+// 逆序
+void reverse(Clink& link)
+{
+	Node* head_ = link.head;
+	if (head_->next == nullptr)
+	{
+		return;
+	}
+	Node* p = head_->next;
+	head_->next = nullptr;
+	while (p != nullptr)
+	{
+		Node* q = p->next; // 下一节点
+		p->next = head_->next;
+		head_->next = p;
+		p = q;
+	}
+}
+
+// 求倒数第k个节点的值
+bool countdown(int k ,Clink& link, int& val)
+{
+	if (k < 1)
+	{
+		return false;
+	}
+	Node* head_ = link.head;
+	Node* p = head_->next;
+	while (k--)
+	{
+		p = p->next;
+		if (p == nullptr)
+		{
+			return false;
+		}
+	}
+	Node* q = head_->next;
+	while (p != nullptr)
+	{
+		p = p->next;
+		q = q->next;
+	}
+	val = q->data;
+	return true;
+}
 
 
 int main()
@@ -125,8 +174,7 @@ int main()
 		int num = rand() % 100;
 		link.inserthead(num);
 	}
-	cout << endl;
-	//link.show();
+	link.show();
 	//link.inserthead(8);
 	//link.show();
 	//link.remove(8);
@@ -138,9 +186,19 @@ int main()
 	//link.show();
 	//link.removeall(999);
 	//link.show();
-	link.inserttail(666);
-	link.show();
-	cout << link.find(666) << endl;
-	cout << link.find(888) << endl;
+	//link.inserttail(666);
+	//link.show();
+	//cout << link.find(666) << endl;
+	//cout << link.find(888) << endl;
+	//reverse(link);
+	//link.show();
+	
+	int kval;
+	int k = 5;
+
+	if (countdown(k, link, kval))
+	{
+		cout << "倒数" << k << "个值为：" << kval << endl;
+	}
 	return 0;
 }
