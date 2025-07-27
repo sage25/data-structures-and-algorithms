3ca965eb24d49a935381d510c92021d49dac2525
muyi-2580 <m19112386564@163.com>
2025-07-27 21:24:19 +0800

    分治算法

--- DivideAndGovern/DivideAndGovern/test.cpp
+++ DivideAndGovern/DivideAndGovern/test.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<iostream>
 #include<math.h>
+#include<algorithm>
 using namespace std;
 
 #if 0
@@ -75,35 +76,114 @@ int main()
 #endif
 
 //求两个有序数组的中位数，时间复杂度为log(n+m)
-int func(int arr[], int i, int j, int brr[], int l, int r, int k)
+double func(int arr[], int brr[], int n, int m)
 {
-	if (arr[i + 1] > brr[l] && brr[l + 1] > arr[i])
+	if (n > m)
 	{
-		return max(arr[i], brr[l]);
+		return func(brr, arr, m, n);
+	}
+	if (n == 0)
+	{
+		int k = (m - 1) / 2;
+		if (m % 2 == 0)
+		{
+			return (brr[k] + brr[k + 1]) * 1.0 / 2;
+		}
+		else
+		{
+			return brr[k];
+		}
+	}
+	
+
+	int k = (n + m + 1) / 2;
+	int i = 0;
+	int j = 0;
+	int begin = 0;
+	int end = n;
+	while (true)
+	{
+		i = (begin + end) / 2;
+		j = k - i;
+		if (i > 0 && j < m && arr[i - 1] > brr[j])
+		{
+			end = i - 1;
+		}
+		else if (j > 0 && i < n && brr[j - 1] > arr[i])
+		{
+			begin = i + 1;
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	int left = 0;
+	if (i == 0)
+	{
+		left = brr[j - 1];
+	}
+	else if (j == 0)
+	{
+		left = arr[i - 1];
 	}
 	else
 	{
-		if (arr[i + 1] < brr[l]) //arr往右边走，brr往左边走
-		{
-			return func(arr, (i + j) / 2, j, brr, l, (l + r) / 2, k);
-		}
-		if (brr[l + 1] < arr[i]) //arr往左边走，brr往右边走
-		{
-			return func(arr, i, (i + j) / 2, brr, (l + r) / 2, r, k);
-		}
+		left = max(arr[i - 1], brr[j - 1]);
 	}
+
+	int right = 0;
+	if (i == n)
+	{
+		right = brr[j];
+	}
+	else if (j == m)
+	{
+		right = arr[i];
+	}
+	else
+	{
+		right = min(arr[i], brr[j]);
+	}
+
+	if ((m + n) % 2 == 0)
+	{
+		return (left + right) * 1.0 / 2;
+	}
+	else
+	{
+		return left;
+	}
+
 }
+
 
 int main()
 {
-	int arr[] = { 1,3,5,8,11,17 };
+	int arr[] = { 1,3,5,9,11,17 };
 	int brr[] = { 2,5,8,10,11,14,17,20 };
 	int n = sizeof(arr) / sizeof(arr[0]);
 	int m = sizeof(brr) / sizeof(brr[0]);
-	int k = (m + n + 1) / 2;
 
-	int result = func(arr, 0, n - 1, brr, 0, m - 1, k);
+	double result = func(arr, brr, m, n);
 
+	vector<int> vec;
+
+	for (int i : arr)
+	{
+		vec.push_back(i);
+	}
+	for (int j : brr)
+	{
+		vec.push_back(j);
+	}
+	sort(vec.begin(), vec.end());
+	for (int v : vec)
+	{
+		cout << v << ' ';
+	}
+	cout << endl;
 	cout << "result:" << result << endl;
 	return 0;
 }
