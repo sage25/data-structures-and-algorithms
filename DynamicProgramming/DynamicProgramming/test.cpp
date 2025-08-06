6a023ceb11eb9d2515bc69979ce91736592115ad
muyi-2580 <m19112386564@163.com>
2025-08-06 20:54:09 +0800

    分治算法

--- DynamicProgramming/DynamicProgramming/test.cpp
+++ DynamicProgramming/DynamicProgramming/test.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<math.h>
+#include<vector>
 using namespace std;
 
 
@@ -78,7 +79,7 @@ int main()
 
 #endif
 
-
+#if 0
 /*
 LIS问题：求最长不降（升序）子数组
 状态转移方程：dp[i] = arr[i] >= arr[i - 1] ? dp[i] = dp[i] + 1 : 1
@@ -107,4 +108,147 @@ int main()
 	int result = func(arr, length, dp);
 	cout << "result:" << result << endl;
 	return 0;
+}
+#endif
+
+//LCS:公共子序列问题求解
+string X = "helloworld";
+string Y = "hlweord";
+int len1 = X.size();
+int len2 = Y.size();
+int** dp = nullptr;
+#if 0
+int func(const string& X, int n, const string& Y, int m)
+{
+    if (n < 0 || m < 0)
+    {
+        return 0;
+    }
+
+    if (dp[n][m] != -1)
+    {
+        return dp[n][m];
+    }
+
+    if (X[n] == Y[m])
+    {
+        dp[n][m] = 1 + func(X, n - 1, Y, m - 1);
+        return dp[n][m];
+    }
+    else
+    {
+        int l1 = func(X, n - 1, Y, m); //往上面走
+        int l2 = func(X, n, Y, m - 1); //往左面走
+        dp[n][m] = max(l1, l2);
+        return dp[n][m];
+    }
+}
+int main()
+{
+    dp = new int* [len1];
+    for (int i = 0; i < len1; ++i)
+    {
+        dp[i] = new int[len2];
+        for (int j = 0; j < len2; ++j)
+        {
+            dp[i][j] = 0;
+        }
+    }
+
+    int result = func1(X, len1 - 1, Y, len2 - 1);
+    cout << "result:" << result << endl;
+
+    return 0;
+}
+#endif
+
+#if 0
+int func1(const string& X, int n, const string& Y, int m)
+{
+    for (int i = 1; i <= n + 1; ++i)
+    {
+        for (int j = 1; j <= m + 1; ++j)
+        {
+            if (X[i - 1] == Y[j - 1])
+            {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            }
+            else
+            {
+                int l1 = dp[i - 1][j]; //从上面来
+                int l2 = dp[i][j - 1]; //从左边来
+                dp[i][j] = max(l1, l2);
+            }
+        }
+    }
+    return dp[n + 1][m + 1];
+}
+
+int main()
+{
+    dp = new int* [len1 + 1];
+    for (int i = 0; i < len1 + 1; ++i)
+    {
+        dp[i] = new int[len2 + 1];
+        for (int j = 0; j < len2 + 1; ++j)
+        {
+            dp[i][j] = 0;
+        }
+    }
+
+    int result = func1(X, len1 - 1, Y, len2 - 1);
+    cout << "result:" << result << endl;
+
+    return 0;
+}
+#endif
+
+/*
+三角数组求和问题:求自顶向下数字之和最小的路线
+        [2]
+       [3,4]
+      [6,5,7]
+     [4,1,8,3]
+
+dp[i][j]:i表示行数，j表示列数，dp[i][j]表示从顶部到当前数字之和
+*/
+
+int func(const vector<vector<int>>& vec, int** dp, int n)
+{
+    for (int i = 0; i < vec.size() - 1; ++i)
+    {
+        dp[n][i] = vec[n][i];
+    }
+
+    for (int i = vec.size() - 2; i >= 0; --i)
+    {
+        for (int j = 0; j < vec[i].size(); ++j)
+        {
+            dp[i][j] = min(vec[i][j] + dp[i + 1][j], vec[i][j] + dp[i + 1][j + 1]);
+        }
+    }
+    return dp[0][0];
+}
+
+int main()  
+{  
+   vector<int> v1{ 2 };  
+   vector<int> v2{ 3,4 };  
+   vector<int> v3{ 6,5,7 };  
+   vector<int> v4{ 4,1,8,3 };  
+   vector<vector<int>> vec{ v1,v2,v3,v4 };  
+   int** dp = new int* [vec.size()];  
+   for (int i = 0; i < vec.size(); ++i)  
+   {  
+       dp[i] = new int[vec[i].size()];
+       for (int j = 0; j < vec[i].size(); ++j)  
+       {  
+           dp[i][j] = 0;  
+       }  
+   }  
+   dp[0][0] = vec[0][0];  
+   int result = func(vec, dp, vec.size() - 1);
+   cout << "result:" << result << endl;  
+
+   return 0;  
 }
